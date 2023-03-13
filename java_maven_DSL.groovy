150ee0625532037cf4486b78face9d568fe95780
Marco Antonio AG <marcontonio.98@hotmail.com>
2023-03-13 06:18:16 -0600

    Primera prueba de DSL con Maven Java apps

---
+++ java_maven_DSL.groovy
@@ -0,0 +1,56 @@
+job("Java maven app with DSL"){
+    
+    description("This app will be builded on kenkins with the help of DSL")
+
+    //Where Jenkins will the code have?
+    smc{
+        git("https://github.com/macloujulian/simple-java-maven-app.git", "master") { node ->
+            node / gitConfigName("Marco Antonio Ag")
+            node / gitConfigEmail("marcontonio.98@hotmail.com")
+        }
+    }
+
+    //The nature app and how will build and run it - Javen Maven
+    setps{
+        //Build it
+        maven{
+            mavenInstallation("mavenJenkins")
+            goals("-B -DskipTests clean package")
+        }
+        //Test it
+        maven{
+            mavenInstallation("mavenInstallation")
+            goals("tests")
+        }
+        //Run it
+        shell('''
+            java -jar "/var/jenkins_home/workspace/Java Maven App DSL/target/my-app-1.0-SNASHOT.jar" 
+        ''')
+    }
+
+    //What after the app will do?
+    publishers{
+        archiveArtifacts("target/*.jar")                //Take every .jar generated and make them avalaible in Jenkins
+        archiveJunit("target/surefire-reports/*.xml")   //Take every .xml generated and make them avalaible in Jenkins
+        //Notifiers (email and message)
+        mailer('macloujulian@gmail.com', false, true)
+        slackNotifier {
+            notifyAborted(true)
+            notifyEveryFailure(true)
+            notifyNotBuilt(true)
+            notifyUnstable(false)
+            notifyBackToNormal(true)
+            notifySuccess(true)
+            notifyRepeatedFailure(false)
+            startNotification(false)
+            includeTestSummary(false)
+            includeCustomMessage(false)
+            customMessage(null)
+            sendAs(null)
+            commitInfoChoice('NONE')
+            teamDomain(null)
+            authToken(null)
+        }
+    }
+
+}
